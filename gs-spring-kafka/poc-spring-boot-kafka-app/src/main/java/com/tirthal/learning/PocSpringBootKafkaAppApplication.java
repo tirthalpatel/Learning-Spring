2c94d731cf1ba91fad9465a2e411a79389b24bec
Tirthal Patel <patel.tirthal@gmail.com>
2017-06-25 13:36:35 +0530

    gs-spring-kafka - added standard spring boot app with test classes

---
+++ gs-spring-kafka/poc-spring-boot-kafka-app/src/main/java/com/tirthal/learning/PocSpringBootKafkaAppApplication.java
@@ -0,0 +1,12 @@
+package com.tirthal.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringBootKafkaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringBootKafkaAppApplication.class, args);
+	}
+}
